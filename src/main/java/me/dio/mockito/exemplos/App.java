d91b8145d9b2a2c05fdc1564c4e5603cf10d272c
Fertilesoil <gasnbloop@gmail.com>
2024-07-06 16:57:52 -0300

    Inicio dos testes com mockito

---
+++ src/main/java/me/dio/mockito/exemplos/App.java
@@ -0,0 +1,13 @@
+package me.dio.mockito.exemplos;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
